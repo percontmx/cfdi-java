f53ccc37a5cbb79f197ee6e835878aba2bb96084
Alex Percont <alexpercont@gmail.com>
2020-10-08 18:02:04 -0500

    Relacionar atributo UUID de Comprobante como objeto.

---
+++ v3_3/src/main/java/mx/gob/sat/cfd/v3_3/adapters/UUIDXMLAdapter.java
@@ -0,0 +1,17 @@
+package mx.gob.sat.cfd.v3_3.adapters;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.UUID;
+
+public class UUIDXMLAdapter extends XmlAdapter<String, UUID> {
+
+    @Override
+    public UUID unmarshal(String string) {
+        return null != string ? UUID.fromString(string) : null;
+    }
+
+    @Override
+    public String marshal(UUID uuid) {
+        return null != uuid ? uuid.toString() : null;
+    }
+}
