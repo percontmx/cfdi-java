36aba53f6fcf8d66798e8fcceb4f3e9312a14a43
Alex Percont <alexpercont@gmail.com>
2025-08-05 09:10:57 -0600

    Documentación básica.

--- utils/parsers/src/main/java/io/github/percontmx/cfdi/utils/parsers/ContextDetector.java
+++ utils/parsers/src/main/java/io/github/percontmx/cfdi/utils/parsers/ContextDetector.java
@@ -2,10 +2,27 @@ package io.github.percontmx.cfdi.utils.parsers;
 
 import org.w3c.dom.Document;
 
+/**
+ * Interfaz para detectar el contexto de un documento XML dado.
+ * Las implementaciones de esta interfaz deben definir la lógica para determinar
+ * si el contexto aplica a un documento XML específico y proporcionar la
+ * ruta de contexto correspondiente.
+ */
 public interface ContextDetector {
 
+    /**
+     * Determina si el contexto aplica al documento XML proporcionado.
+     *
+     * @param document el documento XML a evaluar
+     * @return true si el contexto aplica al documento, false en caso contrario
+     */
     boolean apply(Document document);
 
+    /**
+     * Obtiene la ruta de contexto asociada a este detector.
+     *
+     * @return la ruta de contexto como una cadena de texto
+     */
     String getContextPath();
 
 }
