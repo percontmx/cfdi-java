ee04e9a17ed84fbcfc4514b1b716cd2caed9b863
Alex Percont <alexpercont@gmail.com>
2025-08-06 07:00:04 -0600

    Implementación de refactors.

--- utils/parsers/src/test/java/io/github/percontmx/cfdi/utils/parsers/Cfdi32ContextDetectorTest.java
+++ utils/parsers/src/test/java/io/github/percontmx/cfdi/utils/parsers/Cfdi32ContextDetectorTest.java
@@ -2,9 +2,6 @@ package io.github.percontmx.cfdi.utils.parsers;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,13 +11,13 @@ public class Cfdi32ContextDetectorTest extends AbstractContextDetectorTest {
     private static final ContextDetector contextDetectorToTest = new Cfdi32ContextDetector();
 
     @Test
-    public void getContextPathForValidDocument() throws IOException, SAXException {
+    public void getContextPathForValidDocument() {
         Document xmlDoc = loadDocument(XML_TO_TEST);
         assertTrue(contextDetectorToTest.apply(xmlDoc));
     }
 
     @Test
-    public void getContextPathForInvalidDocument() throws IOException, SAXException {
+    public void getContextPathForInvalidDocument() {
         Document xmlDoc = loadDocument("invalido_muestra.xml");
         assertFalse(contextDetectorToTest.apply(xmlDoc));
     }
