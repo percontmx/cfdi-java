adf2d7d8dc86a078bfdff3a8699aae2fe3cd4dfc
Alex Percont <alexpercont@gmail.com>
2026-02-06 22:34:15 -0600

    Agregar soporte para herencia en el mapeo de deducciones en bindings JAXB

---
+++ complementos/nomina-api/src/main/java/io/github/percontmx/cfdi/complementos/nomina/Deduccion.java
@@ -0,0 +1,8 @@
+package io.github.percontmx.cfdi.complementos.nomina;
+
+import mx.gob.sat.cfdi.catalogos.nomina.TipoDeduccion;
+
+public interface Deduccion {
+
+    TipoDeduccion getTipoDeduccion();
+}
