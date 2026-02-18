8015f780569d4c5c7d85e1423202790353d6a165
Alex Percont <alexpercont@gmail.com>
2026-02-18 06:53:30 -0600

    Agregar mapeo de atributo ImporteGravado y método getImporte en la interfaz Deduccion

--- complementos/nomina-api/src/main/java/io/github/percontmx/cfdi/complementos/nomina/Deduccion.java
+++ complementos/nomina-api/src/main/java/io/github/percontmx/cfdi/complementos/nomina/Deduccion.java
@@ -1,10 +1,13 @@
 package io.github.percontmx.cfdi.complementos.nomina;
 
 import mx.gob.sat.cfdi.catalogos.nomina.TipoDeduccion;
+import java.math.BigDecimal;
 
 public interface Deduccion {
 
     TipoDeduccion getTipoDeduccion();
+
+    BigDecimal getImporte();
 }
 
 
